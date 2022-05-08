5a19596c5c6ebe82a2ec420ca3bd4175f319d77e
dhthinh7 <dhthinh7@gmail.com>
2022-05-09 00:20:57 +0700

    Learning biding data, condition biding, state Auhtor: Thinh Doan Status: Learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import LayoutEx from './component/LayoutEx/LayoutEx';
+// import LayoutEx from './component/LayoutEx/LayoutEx';
+// import DataBidings from './DataBinding/DataBidings';
+// import DataBindingRFC from './DataBinding/DataBindingRFC';
+// import HandleEvent from './HandleEvent/HandleEvent';
+// import RederCondition from './RederingCondition/RederCondition';
+import State from './State/State';
 
 function App() {
   return (
     <div className="App">
-      <LayoutEx/>
+      {/* <LayoutEx/> */}
+      {/* Demo databidings */}
+      {/* <DataBidings/>
+      <DataBindingRFC/> */}
+      {/* <HandleEvent/> */}
+      {/* <RederCondition/> */}
+      <State/>
     </div>
   );
 }
