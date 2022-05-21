54eaf0e3fedf446f3721a762cb56bfd8f0119755
dhthinh7 <dhthinh7@gmail.com>
2022-05-22 06:36:49 +0700

    Update excersie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import ChooseCarAdvance from './ChoseCarAdvance/ChooseCarAdvance';
+// import RenderWithLoop from './RederWithLoop/RenderWithLoop';
+// import TryGlassesOn from './TryGlassesOn/TryGlassesOn';
 // import LayoutEx from './component/LayoutEx/LayoutEx';
 // import DataBidings from './DataBinding/DataBidings';
 // import DataBindingRFC from './DataBinding/DataBindingRFC';
 // import HandleEvent from './HandleEvent/HandleEvent';
 // import RederCondition from './RederingCondition/RederCondition';
 // import State from './State/State';
-import ChooseCar from './ChoseCarEx/ChooseCar';
+// import ChooseCar from './ChoseCarEx/ChooseCar';
 
 function App() {
   return (
@@ -18,7 +21,10 @@ function App() {
       {/* <HandleEvent/> */}
       {/* <RederCondition/> */}
       {/* <State/> */}
-      <ChooseCar/>
+      {/* <ChooseCar/> */}
+      {/* <RenderWithLoop/> */}
+      {/* <TryGlassesOn/> */}
+      <ChooseCarAdvance/>
     </div>
   );
 }
