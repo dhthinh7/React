67575788b66078ef0cf87cc71619e57f8d66c4f9
dhthinh7 <dhthinh7@gmail.com>
2022-08-15 23:26:24 +0700

    Add code react_lifecyle

---
+++ Course_3_ReactLifeCycleHook/Learning/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import UserProfile from './FormValidation/UserProfile/UserProfile'
+import DemoJSS from './JSS_StyledComponent/DemoJSS/DemoJSS'
+import DemoTheme from './JSS_StyledComponent/Themes/DemoTheme'
+import ToDoList from './JSS_StyledComponent/BaiTapStyleComponent/ToDoList/ToDoList'
+import LifeCycleReact from './LifeCycleReact/LifeCycleReact'
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+          {/* <UserProfile /> */}
+          {/* <DemoJSS /> */}
+          {/* <DemoTheme /> */}
+          <ToDoList />
+          {/* <LifeCycleReact /> */}
+      </div>
+    )
+  }
+}
